da8b9659e304f3f89a5165c22609748b98c6e686
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-30 16:33:45 +0530

    code starts

--- bus-booking/src/Bus/project/Bus.java
+++ bus-booking/src/Bus/project/Bus.java
@@ -1,8 +1,38 @@
 package Bus.project;
+import java.util.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+class DbConnection{
+	static String url="jdbc:mysql://localhost:3306/busbooking";
+	static String user ="root";
+	static String password = "Mathi.51228";
+	public static  Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url,user,password);
+		
+	}
+	
+}
+class BusDao{
+	public static void details() throws SQLException {
+		Connection con = DbConnection.getConnection();
+		Statement st = con.createStatement();
+		String q ="select * from bus";
+		ResultSet rst = st.executeQuery(q);
+		while(rst.next()) {
+			System.out.println(" BUS NO "+rst.getInt(1)+" CAPACITY "+rst.getInt(2)+" AC "+rst.getBoolean(3));
+			
+		}
+;	}
+}
 
 public class Bus {
-	public static void main (String args[]) {
-		System.out.println("hello");
+	public static void main (String args[]) throws SQLException {
+		BusDao.details();
 	}
 
 }
--- bus-booking/src/module-info.java
+++ bus-booking/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module BusBooking {
+	requires java.sql;
 }
